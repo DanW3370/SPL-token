8daab1998ecf9b24d51aa9600e5d8a8c00881acb
DanW3370 <47480282+DanW3370@users.noreply.github.com>
2026-02-24 15:03:04 +0800

    Update connection.ts

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -1,1 +1,7 @@
+import { Connection, clusterApiUrl } from "@solana/web3.js";
+import { RPC } from "../config.js";
 
+export function getConnection(): Connection {
+  const endpoint = RPC || clusterApiUrl("devnet");
+  return new Connection(endpoint, "confirmed");
+}
